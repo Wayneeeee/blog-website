3661a4c189966a67721add07b55ce36e8bfba2be
Wayne <zwzwkl@hotmail.com>
2023-11-02 22:56:58 +0800

    icon update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
         title: '片面 Crypto',
         logo: {
           alt: '片面 Crypto',
-          src: 'img/wayne.jpg',
+          src: 'img/wayne.png',
         },
         items: [
           {
